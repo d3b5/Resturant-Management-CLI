ad74de95d0e1ecb29fb050dcf43cb323a3a7f623
deb <debaleenasarker2004@gmail.com>
2026-03-15 20:06:56 +0600

    Rename view_employee method to view_employees

--- users.py
+++ users.py
@@ -33,7 +33,7 @@ class Admin(User):
     def __init__(self, name, phone, email, address):
         super().__init__(name, phone, email, address)
             
-    def view_employee(self):
+    def view_employees(self):
         print("Employee List:")
         for i,emp in enumerate(self.employees,1):
             print(f"{i}.{emp}")
