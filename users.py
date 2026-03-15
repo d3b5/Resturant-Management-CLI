9ef15afba44b4e3dc91d05c26a2d7df17f8f0257
deb <debaleenasarker2004@gmail.com>
2026-03-15 19:49:51 +0600

    Add veiw_employee method to Admin

--- users.py
+++ users.py
@@ -38,3 +38,9 @@ class Admin(User):
         employee = Employee(name, phone, email, address, age, designation, salary)
         self.employees.append(employee)
         print(f"{name} is added as an employee!")
+    
+    def view_employee(self):
+        print("Employee List:")
+        for i,emp in enumerate(self.employees,1):
+            print(f"{i}.{emp}")
+
