50d93bc93979990b0ac3ed307747713572e8bbac
deb <debaleenasarker2004@gmail.com>
2026-03-15 19:20:27 +0600

    Add Employee subclass

--- users.py
+++ users.py
@@ -12,4 +12,9 @@ class User(ABC):
         self.email = email
         self.address = address
 
-
+class Employee(User):
+    def __init__(self, name, phone, email, address, age, designation, salary):
+        super().__init__(name, phone, email, address)
+        self.age = age
+        self.designation = designation
+        self.salary = salary
