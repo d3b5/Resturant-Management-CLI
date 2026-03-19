c302563f8b19027b462e63afda281a10a0c885f2
deb <debaleenasarker2004@gmail.com>
2026-03-19 22:52:24 +0600

    Show message if there are no employees in view_employees method in Restaurant

--- restaurant.py
+++ restaurant.py
@@ -8,9 +8,12 @@ class Restaurant:
 
     def add_employee(self, employee):
         self.employees.append(employee)
-        print(f"{employee.name} is added as an employee!")
+        print(f"{employee.name.title()} is added as an employee!")
         
     def view_employees(self):
-        print("Employee List:")
-        for i,emp in enumerate(self.employees,1):
-            print(f"{i}.{emp}")
+        if not self.employees:
+            print("There are no employees!")
+        else:
+            print("Employee List:")
+            for i,emp in enumerate(self.employees,1):
+                print(f"{i}.{emp}")
