8e984a00321e06396f9883afd3e87c4b063e8783
deb <debaleenasarker2004@gmail.com>
2026-03-19 23:33:05 +0600

    Fix typo in Menu

--- menu.py
+++ menu.py
@@ -5,7 +5,7 @@ class Menu:
     
     def add_item(self,item):
         self.items[item.name]=item
-        print(f"{item.name} added successfully!")
+        print(f"{item.name.title()} added successfully!")
     
     def find_item(self,item_name):
         return self.items.get(item_name) #returns item or None
@@ -13,7 +13,7 @@ class Menu:
     def remove_item(self,item_name): 
         if self.find_item(item_name) is not None: 
             self.items.pop(item_name)
-            print(f"{item.name} removed successfully!")
+            print(f"{item_name.title()} removed successfully!")
         else:
             print("Invalid Item")
 
@@ -23,7 +23,7 @@ class Menu:
             print("Menu is Empty!")
         else:            
             print("<-------MENU------->")
-            print(f"   Name\tPrice\tQuantity")
+            print(f"    Name\tPrice\tQuantity")
             
             for i,item in enumerate(self.items.values(),1):            
                 print(f"{i}. {item.name.title()}\t{item.price}\t{item.quantity}")
