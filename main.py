fb83db9813b82daff25665dcfee62ca05733ae2f
deb <debaleenasarker2004@gmail.com>
2026-03-20 23:54:42 +0600

    Use get_input for all inputs to ensure valid inputs

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from restaurant import Restaurant
 from menu import Menu
 from food_item import FoodItem
 from order import Order
+from helper import get_input, name_validator, int_validator, float_validator
 
 star = Restaurant("Star Hotel")
 
 def customer_menu():
-    name = input("Enter Your Name: ").lower()
-    email = input("Enter Your E-Mail: ").lower()
-    phone = input("Enter Your Phone Number: ")
-    address = input("Enter Your Address: ").lower()
+    name = get_input(prompt="Enter your name: ", validator = name_validator, error_message="Valid Name is required.").lower()
+
+    email = get_input(prompt="Enter your e-mail: ", validator = lambda x: '@' in x and '.' in x.split('@')[-1], error_message="Valid Email is required.").lower()
+
+    phone = get_input(prompt="Enter your phone number: ", validator = lambda x: x.strip() != '', error_message="Valid Phone number is required.")
+
+    address = get_input(prompt="Enter your address: ", validator = lambda x: x.strip() != '', error_message="Valid Address is required.").lower()
+
     customer = Customer(name = name, phone = phone, email = email, address = address)
 
     print(f"Welcome {customer.name.title()}!")
@@ -25,19 +30,19 @@ def customer_menu():
         6. Pay Bill
         7. Exit
         """)
-        choice = int(input("Choice: "))
+        choice = get_input(prompt="Choice: ",cast_func=int, validator=int_validator,error_message="Invalid Choice!")
         if choice == 1:
             customer.show_menu(restaurant=star)
         elif choice == 2:
             item_name = input("Item Name: ").lower()
-            quantity = int(input("Quantity: "))
+            quantity = get_input(prompt="Quantity: ",cast_func=int, validator=int_validator,error_message="Enter valid quantity.")
             customer.add_to_cart(restaurant = star, item_name = item_name, quantity = quantity)
         elif choice == 3:
             if customer.cart_empty:
                 print("Sorry, your cart is empty!")
             else:
                 item_name = input("Item Name: ").lower()
-                quantity = int(input("Quantity: "))
+                quantity = get_input(prompt="Quantity: ",cast_func=int, validator=int_validator,error_message="Enter valid quantity.")
                 customer.remove_from_cart(restaurant = star, item_name = item_name, quantity = quantity)
         elif choice == 4:
             customer.show_cart()
@@ -45,11 +50,11 @@ def customer_menu():
             print(f"Total Bill: {customer.show_bill} USD")
             choice = input("Would you like to pay now? (y/n): ").lower()
             if choice == 'y':
-                cash = int(input("Enter Bill: "))
+                cash = get_input(prompt="Enter Bill: ",cast_func=int, validator=int_validator,error_message="Enter valid amount.")
                 customer.pay_bill(cash)
         elif choice == 6:
             print(f"Total Bill: {customer.show_bill}")
-            cash = int(input("Enter Bill: "))
+            cash = get_input(prompt="Enter Bill: ",cast_func=int, validator=int_validator,error_message="Enter valid amount.")
             customer.pay_bill(cash)
         elif choice == 7:
             print(f"Thank you for visiting {star.name}\n")
@@ -58,10 +63,14 @@ def customer_menu():
             print("Invalid Choice!")
 
 def admin_menu():
-    name = input("Enter Your Name: ").lower()
-    email = input("Enter Your E-Mail: ").lower()
-    phone = input("Enter Your Phone Number: ")
-    address = input("Enter Your Address: ").lower()
+    name = get_input(prompt="Enter your name: ", validator = name_validator, error_message="Valid Name is required.").lower()
+
+    email = get_input(prompt="Enter your e-mail: ", validator = lambda x: '@' in x and '.' in x.split('@')[-1], error_message="Valid Email is required.").lower()
+
+    phone = get_input(prompt="Enter your phone number: ", validator = lambda x: x.strip() != '', error_message="Valid Phone number is required.")
+
+    address = get_input(prompt="Enter your address: ", validator = lambda x: x.strip() != '', error_message="Valid Address is required.").lower()
+
     admin = Admin(name = name, phone = phone, email = email, address = address)
 
     print(f"Welcome Admin {admin.name.title()}!")
@@ -75,11 +84,11 @@ def admin_menu():
         5. View menu
         6. Exit
         """)
-        choice = int(input("Choice: "))
+        choice = get_input(prompt="Choice: ",cast_func=int, validator=int_validator,error_message="Invalid Choice!")
         if choice == 1:
             item_name = input("Item Name: ").lower()
-            quantity = int(input("Quantity: "))
-            price = float(input("Price: "))
+            quantity = get_input(prompt="Quantity: ",cast_func=int, validator=int_validator,error_message="Enter valid quantity.")
+            price = get_input(prompt="Price: ",cast_func=float, validator=float_validator,error_message="Enter valid price.")
             item = FoodItem(name = item_name, price = price, quantity = quantity)
             admin.add_item(star,item)
         elif choice == 2:
@@ -87,13 +96,18 @@ def admin_menu():
             admin.remove_item(star,item_name)
         elif choice == 3:
             print("Enter employee details:")
-            name = input("Name: ").lower()
-            email = input("E-Mail: ").lower()
-            phone = input("Phone Number: ")
-            address = input("Address: ").lower()
-            age = int(input("Age: "))
-            designation = input("Designation: ")
-            salary = int(input("Salary: "))
+
+            name = get_input(prompt="Enter name: ", validator = name_validator, error_message="Valid Name is required.").lower()
+
+            email = get_input(prompt="Enter e-mail: ", validator = lambda x: '@' in x and '.' in x.split('@')[-1], error_message="Valid Email is required.").lower()
+
+            phone = get_input(prompt="Enter phone number: ", validator = lambda x: x.strip() != '', error_message="Valid Phone number is required.")
+
+            address = get_input(prompt="Enter address: ", validator = lambda x: x.strip() != '', error_message="Valid Address is required.").lower()
+
+            age = get_input(prompt="Enter age: ",cast_func=int, validator=int_validator, error_message="Enter valid age.")
+            designation = get_input(prompt="Enter designation: ",cast_func=str, validator= lambda x: x.strip() != '', error_message="Enter valid designation")
+            salary = get_input(prompt="Enter salary: ",cast_func=int, validator=int_validator, error_message="Enter valid salary.")
             employee = Employee(name=name, phone = phone, email = email, address = address, age = age, designation = designation, salary = salary)
             admin.add_employee(star,employee)
         elif choice == 4:
@@ -112,7 +126,7 @@ while True:
     1. Admin
     2. Customer
     3. Exit """)
-    choice = int(input("Choice: "))
+    choice = get_input(prompt="Choice: ",cast_func=int, validator=int_validator,error_message="Invalid Choice!")
     if choice == 1:
         admin_menu()
     elif choice == 2:
